e7d8ecafd1dd6e2df7b3b3d079a7ea77787fcf28
Flavelino2021 <89366568+Flavelino2021@users.noreply.github.com>
2021-10-25 00:02:23 -0300

    Add files via upload

---
+++ 03-Interfaces/3.1/figures/Figure.java
@@ -0,0 +1,25 @@
+package figures;
+
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import ivisible.IVisible;
+
+
+public abstract class Figure implements IVisible{
+      public int x, y;
+      public int w, h;
+      public Color contorno, fundo;
+      
+      public void drag (int dx, int dy) {
+        x += dx;
+        y += dy;
+    }
+
+public boolean clicked (int x, int y) {
+        return (this.x<=x && x<=this.x+this.w && this.y<=y && y<=this.y+this.h);
+    }
+    public abstract void paint (Graphics g, boolean focused);
+    
+      
+}
